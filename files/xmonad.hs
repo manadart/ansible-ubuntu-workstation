6898276b37bbb793d1c0ec3347be95730777020a
Joseph Phillips <joe@manalabs.eu>
2026-03-11 08:44:04 +0100

    Fixes deprecation warnings from xmonad.

--- files/xmonad.hs
+++ files/xmonad.hs
@@ -11,27 +11,26 @@ import XMonad.Layout.NoBorders
 doFloatNoFocus :: ManageHook
 doFloatNoFocus = doFloat <+> doF W.focusDown
 
+myManageHook :: ManageHook
 myManageHook = composeAll
     [ className =? "Xmessage" --> doFloatNoFocus
     , className =? "Alert" --> doFloatNoFocus
     , stringProperty "WM_WINDOW_ROLE" =? "alert" --> doFloatNoFocus
-    , manageDocks
     ]
 
+main :: IO ()
 main = do
     xmproc <- spawnPipe "xmobar"
 
-    xmonad $ defaultConfig
+    xmonad . docks $ def
         { modMask = mod4Mask
         , focusedBorderColor = "#7289da"
         , workspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-        , manageHook = myManageHook <+> manageHook defaultConfig
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
-        -- Keep Xmobar visible.
-        , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
+        , manageHook = myManageHook <+> manageHook def
+        , layoutHook = avoidStruts $ layoutHook def
         , logHook = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc
-            , ppTitle = xmobarColor "green" "".shorten 50
+            , ppTitle = xmobarColor "green" "".shorten 70
             }
         } `additionalKeysP`
         [ ("<XF86AudioLowerVolume>", spawn "amixer -D pulse sset Master 5%-")
@@ -39,8 +38,8 @@ main = do
         , ("<XF86AudioMute>", spawn "amixer -D pulse set Master 1+ toggle")
         , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10")
         , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
-        , ("M-<Tab>", moveTo Next NonEmptyWS)
-        , ("M-S-<Tab>", moveTo Prev NonEmptyWS)
+        , ("M-<Tab>", moveTo Next $ Not emptyWS)
+        , ("M-S-<Tab>", moveTo Prev $ Not emptyWS)
         , ("M-0", windows $ W.greedyView "0")
         , ("M-S-0", windows $ W.shift "0")
         ]
